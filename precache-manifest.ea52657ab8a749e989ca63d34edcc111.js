5a73d7bddec92b4cdbb50081049f3fdf0de78364
manish <manish01.it@gmail.com>
2020-11-29 23:19:39 +0530

    Updates

---
+++ precache-manifest.ea52657ab8a749e989ca63d34edcc111.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d2ac7c5d9761f5a0d46520c9508a9be",
+    "url": "/manishbisht-1/amazon_clone.git/index.html"
+  },
+  {
+    "revision": "903757abf3e5df232516",
+    "url": "/manishbisht-1/amazon_clone.git/static/css/main.ff3b7a0a.chunk.css"
+  },
+  {
+    "revision": "c0940972bcfc2d73a5dd",
+    "url": "/manishbisht-1/amazon_clone.git/static/js/2.d1117d03.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/manishbisht-1/amazon_clone.git/static/js/2.d1117d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "903757abf3e5df232516",
+    "url": "/manishbisht-1/amazon_clone.git/static/js/main.785ebc09.chunk.js"
+  },
+  {
+    "revision": "c01971866ce07087950d",
+    "url": "/manishbisht-1/amazon_clone.git/static/js/runtime-main.ed52cbb6.js"
+  },
+  {
+    "revision": "e34f255bdb9eb818c3f7331e7b8944a3",
+    "url": "/manishbisht-1/amazon_clone.git/static/media/ama2.e34f255b.png"
+  },
+  {
+    "revision": "d4f041cfc66f770856548e9902ee6979",
+    "url": "/manishbisht-1/amazon_clone.git/static/media/bannerImage.d4f041cf.jpg"
+  }
+]);
